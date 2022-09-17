905873dd19b5707c21b790a6e5842a7e342331f0
dagwan <dagwanpan@gmail.com>
2022-09-17 20:31:02 +0100

    adjustment

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,7 +1,7 @@
 
 //sellecting items
 const btn = document.getElementById("button");
-const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'rebeccapurple', 'violet'];
+const randomColor = ["#8758FF", "#181818", "red", "orange", "yellow", "green", "blue", "#CD104D"];
 
 btn.addEventListener("click", function () {
     change()
@@ -9,7 +9,7 @@ btn.addEventListener("click", function () {
 
 //function to change background using the collors in the array.
 function change() {
-    document.body.style.background = rainbow[Math.floor(Math.random() * rainbow.length)];
+    document.body.style.background = randomColor[Math.floor(Math.random() * randomColor.length)];
 };
 
 
