6d5f586a024dfab8b954b1364a2899e7de06a18c
dagwan <dagwanpan@gmail.com>
2022-10-29 12:40:00 +0100

    assignment

--- lesson7/todo/main.js
+++ lesson7/todo/main.js
@@ -1,26 +1,32 @@
-const form = document.forms["todo"];
-
-form.addEventListener("submit", addTask, false);
+const form = document.forms['todo'];
+form.addEventListener('submit', addTask, false);
 
 function addTask(event) {
     event.preventDefault();
-    const tak = new FormData(form);
-    const url = "https://jsonplaceholder.typicode.com/todos";
-    const header = new Headers({
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    });
+    const number = form.task.value;
+    const task = {
+        userId: 1,
+        title: form.task.value,
+        completed: false
+    }
+    const data = JSON.stringify(task);
+    const url = 'https://jsonplaceholder.typicode.com/todos';
 
-    const request = new Request(url, {
-        method: "POST",
-        mode: "cors",
-        header: Headers,
-        body: JSON.stringify(task)
+    const headers = new Headers({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    });
+    const request = new Request(url,
+    {
+        method: 'POST',
+        header: headers,
+        body: data
     }
     )
 
     fetch(request)
-    .then(response => response.json())
-    .then(data => console.log(`${data.title}Task saved with an id of ${data.id}`))
-    .catch(error => console.log("There was an error:", error))
+    .then( response => response.json() )
+    .then( task => console.log(`Task saved with an id of ${task.id}`) )
+    .catch( error => console.log('There was an error:', error))
+
 }
